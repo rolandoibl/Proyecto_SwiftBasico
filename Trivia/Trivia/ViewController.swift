26ce6c87e74c92e574b835e78e99c7c9fc51ca9f
rolandoibl <rolandoil@hotmail.com>
2020-01-23 12:59:46 -0600

    Add files via upload
    Primera edición con interfaz gráfica

---
+++ Trivia/Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by 2020-2 on 1/23/20.
+//  Copyright © 2020 Conde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
